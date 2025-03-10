76bd4e908ed91dca835d153deb89edc68e7cef83
Joe Chirchirillo <cchirchirillo3@gmail.com>
2025-03-10 17:51:56 -0600

    Chore: component creation for dash, all notes, create note

---
+++ src/app/note-create/note-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-create',
+  imports: [],
+  templateUrl: './note-create.component.html',
+  styleUrl: './note-create.component.css'
+})
+export class NoteCreateComponent {
+
+}
