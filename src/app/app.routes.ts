72103580785fa12b168d41e5264490844f8d7c56
Joe Chirchirillo <cchirchirillo3@gmail.com>
2025-03-10 18:03:08 -0600

    Fix: restructures route imports, break for night

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NoteListComponent } from './note-list/note-list.component'
+import { NoteCreateComponent } from './note-create/note-create.component';
 
 export const routes: Routes = [
-  { path: '', component: LandingComponent },
-
+  { path: '', component: LandingComponent }, //default route
+  { path: 'dashboard', component: DashboardComponent, //parent
+    children: [                                           
+      { path: '', redirectTo: 'all', pathMatch: 'full' }, // all pathes after parent. LH4200/dashboard/....
+      { path: 'all', component: NoteListComponent },
+      { path: 'subject/:id', component: NoteListComponent },
+      { path: 'new', component: NoteCreateComponent }
+    ]
   }
 ];
