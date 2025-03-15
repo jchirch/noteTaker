e0e7c0b3503b29b2c3d7c034c23ed7ad70b7eef2
Joe Chirchirillo <cchirchirillo3@gmail.com>
2025-03-15 14:44:58 -0600

    Fix: changed app html to own file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,7 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterModule], // import RouterModule
   selector: 'app-root',
-  template: `
-    <header>
-      <div class="logo-container">
-        <h1 class="header">Note</h1>
-        <h1 class="header">Taker</h1>
-      </div>
-      <nav>
-        <a routerLink="/">Home</a>
-        <a routerLink="/dashboard">Dashboard</a>
-        <a routerLink="/dashboard/notes">All Notes</a>
-        <a routerLink="/dashboard/new">Create Note</a>
-      </nav>
-    </header>
-    <router-outlet></router-outlet>
-    <footer>
-      <p>Created by Joe Chirchirillo</p>
-      <a href="https://www.linkedin.com/in/joechirchirillo/" target="_blank">LinkedIn</a>
-      <a href="https://github.com/joechirchirillo" target="_blank">GitHub</a>
-      <a href="https://github.com/jchirch/noteTaker" target="_blank">App Repo</a>
-    </footer> 
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent { }
