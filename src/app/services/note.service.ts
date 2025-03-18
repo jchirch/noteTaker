7b6cab1764029a0f5dad05e71329234d39c27031
Joe Chirchirillo <cchirchirillo3@gmail.com>
2025-03-18 12:35:42 -0600

    Chore: outlining Note service

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Note { // interface defines structure of a note
+  id: number;
+  title: string;
+  content: string;
+  created_at: string;
+  updated_at: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class NoteService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/api/v1'; // switch out url when deployed
+  constructor(private http: HttpClient) { } //injects http client so service can mke http requests
+
+  getAllNotes(): Observable<{ Note }> { //function return an observable object
+    return this.http.get(`${this.apiUrl}/notes`); // returns {count: ###} from apiURL route request
+  }
 }
