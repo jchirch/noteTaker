a406c38346d23b183681f69214e4610d91787f8d
Joe Chirchirillo <cchirchirillo3@gmail.com>
2025-03-21 17:06:10 -0600

    Chore: set up form submit fuction

--- src/app/note-create/note-create.component.ts
+++ src/app/note-create/note-create.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; // define angular componenet
+import { FormBuilder, FormGroup, Validators } from '@angular/forms' // reactive froms
+import { ReactiveFormsModule } from '@angular/forms'; // reactive forms vs template form
+import { Router } from '@angular/router'; //router
+import { CommonModule } from '@angular/common'; // directives ngIf/ngFor
+import { NoteService } from '../services/note.service'; // my premade service to inject Notes content into componenet
+ 
 
 @Component({
   standalone: true,
   selector: 'app-note-create',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './note-create.component.html',
   styleUrl: './note-create.component.css'
 })
 export class NoteCreateComponent {
+  noteForm: FormGroup
+  errorMsg: string | null = null;
 
+  constructor( private fb: FormBuilder, private noteService: NoteService, private router: Router){
+    this.noteForm = this.fb.group({
+      title: [''],
+      content: ['']
+    })
+  }
+
+  onSubmit(): void {
+    if (this.noteForm.valid) { //if form is created, proceed
+      this.noteService.createNote(this.noteForm.value).subscribe({ // calls on instance to inject create form function
+        next: () => this.router.navigate(['/dashboard/notes']), // if success, submit form and route to all notes
+        error: (error) => {
+          console.log('Error creating new note: ', error)
+          this.errorMsg = 'Failure creating new note.'
+        }
+      })
+    }
+  }
 }
